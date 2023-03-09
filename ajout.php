a0286501b70ee3458e16752f60de53d3aa4d85c7
Stelian-Constantin Stanciu <stanciu.stelian86@yahoo.fr>
2023-03-09 15:10:38 +0100

    création requete pour ajouter un film dans l'sql

--- ajout.php
+++ ajout.php
@@ -3,3 +3,29 @@ include './inc/fonctions.php';
 include './inc/pdo.php';
 include './partials/header.php';
 ?>
+<?php
+$sqlRequest = "INSERT INTO movies (title, slug, genres, directors, year) VALUES (:title, :director, :year)";
+
+// Préparation de la requête SQL
+$resultat = $conn->prepare($sql);
+
+// Liaison des paramètres avec les valeurs du nouveau film
+$resultat->bindValue(':title', $title, PDO::PARAM_STR);
+$resultat->bindValue(':slug', $year, PDO::PARAM_INT);
+$resultat->bindValue(':genres', $year, PDO::PARAM_INT);
+$resultat->bindValue(':directors', $director, PDO::PARAM_STR);
+$resultat->bindValue(':year', $year, PDO::PARAM_INT);
+
+
+// Exécution de la requête SQL
+$resultat->execute();
+
+// Vérification du nombre de lignes affectées (1 si l'ajout a réussi, 0 sinon)
+if ($resultat->rowCount() > 0) {
+    echo "Le nouveau film a été ajouté avec succès !";
+} else {
+    echo "Le nouveau film n'a pas pu être ajouté.";
+}
+
+// Fermeture de la connexion à la base de données
+$pdo = null;
